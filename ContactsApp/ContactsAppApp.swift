e595b385c6ff2280c08056493f4c42a07f88d5a7
maciejeicam <maciej_michalik@onet.pl>
2023-04-05 14:12:34 +0200

    Initial Commit

---
+++ ContactsApp/ContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppApp.swift
+//  ContactsApp
+//
+//  Created by Maciej Michalik on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
